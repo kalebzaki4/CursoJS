c878f7590f58cf4261ab4c6b29f36582bf20ee93
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-06-19 01:09:10 -0300

    Aulas do ex008!
    Retirei o else, pois é desnecessario por enquanto!

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -3,6 +3,5 @@ console.log(`A velocidade do seu carro é ${vel}Km/h`)
 if (vel > 60) { //Condição simples!
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 } else {
-    console.log(`Você esta na velocidade permitida!`)
-    console.log(`Dirija usando sempre o cinto de segurança!`)
+
 }
