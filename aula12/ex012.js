0826d1e3f0e33a0c0097a79dac606e71f682842d
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-06-20 12:27:07 -0300

    condicao aninhada
    condicao aninhada na pasta 012

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,8 +1,9 @@
-var hora = 19
+var agora = new Date()
+var hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas`)
 if (hora < 12) {
     console.log('Bom dia!')
-} else if (hora < 18 && hora > 12) {
+} else if (hora <= 18) {
     console.log('Boa Tarde!')
 } else {
     console.log('Boa Noite!')
