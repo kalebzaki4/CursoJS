ab00fcb66c712720260103996f6a42b659273efb
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-06-24 14:11:29 -0300

    new date no script do ex015
    Adicionando new date no script do ex015

--- Aula12ex/Ex015/script.js
+++ Aula12ex/Ex015/script.js
@@ -1,3 +1,21 @@
 function verificar() {
-    
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fAno.value.lenght == 0 || fAno.value > ano ) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        var genero = ''
+        
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } else if (fsex[1].checked) {
+             genero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    } 
 }
