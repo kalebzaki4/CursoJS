6ea5151d0f5a0d5bbda67ae7a4a395909b1d1203
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-06-29 10:10:41 -0300

    function tabuada
    adicionei function tabuada

--- Aula14Ex/Ex17/script.js
+++ Aula14Ex/Ex17/script.js
@@ -1,49 +1,19 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fAno = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    if (fAno.value.lenght == 0 || fAno.value > ano ) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tap = document.getElementById('seltab')
+    if (num.value.length     == 0) {
+        window.alert('Por favor, digite um numero!')
     } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fAno.value)
-        var genero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if (fsex[0].checked) {
-            genero = 'Homem'
-            if (idade >=0 && idade < 10) {
-                // Criança!
-                img.setAttribute('src', 'foto-bebe-m.png')
-            } else if (idade >= 10 && idade < 18) {
-                // jovem
-                img.setAttribute('src', 'foto-jovem-m.png')
-            } else if (idade >= 18 && idade < 61) {
-                // Adulto
-                img.setAttribute('src', 'foto-adulto-m.png')
-            } else {
-                // Idoso
-                img.setAttribute('src', 'foto-idoso-m.png')
-            }
-        } else if (fsex[1].checked) {
-             genero = 'Mulher'
-             if (idade >=0 && idade < 10) {
-                // Criança!
-                img.setAttribute('src', 'foto-bebe-f.png')
-            } else if (idade >= 10 && idade < 18) {
-                // jovem
-                img.setAttribute('src', 'foto-jovem-f.png')
-            } else if (idade >= 18 && idade < 61) {
-                // Adulto
-                img.setAttribute('src', 'foto-adulto-f.png')
-            } else {
-                // Idoso
-                img.setAttribute('src', 'foto-idoso-f.png')
-            }
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
         }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        res.appendChild(img)
-    } 
+    }
+
 }
